3fbd2fbe886d61ab60131247d5ada90292f315fe
Robert Smith <smithrobertlawrence@gmail.com>
2014-08-27 21:11:35 -0400

    Created Repo, added main script
    main script is run by Google App Engine, pulls data from websites and
    creates mined-per-day rankings

---
+++ coin_data.py
@@ -0,0 +1,280 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import time
+import webapp2
+import cgi
+import urllib2
+import re
+import jinja2
+from string import letters
+import string
+import hashlib
+import hmac
+import json
+import random
+from google.appengine.ext import db
+from google.appengine.api import memcache
+from xml.dom import minidom
+import logging
+import pickle
+import operator
+import sys
+sys.path.insert(0, 'requests')
+import requests
+from requests.exceptions import ConnectionError
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+SECRET = "correcthorsebatterystaple0110100110010110"
+BLOCKS_PER_DAY = {"LTC":576, "BTC":144, "AUR":288, "DOGE":1440, "PPC":144, "NMC":144, "FTC":1440}
+COIN_URLS = {"BTC":"http://www.bitcoin.org/","LTC":"http://litecoin.org/", "DOGE":"http://dogecoin.com/",
+"VTC":"http://vertcoin.org/", "PPC":"http://www.peercoin.net/", "NMC":"http://namecoin.info/",
+"MAX":"http://www.maxcoin.co.uk/", "FTC":"http://feathercoin.com/", "XPM":"http://primecoin.io/",
+"DRK":"http://www.darkcoin.io/", "WDC":"http://www.worldcoinfoundation.org/", "NVC":"http://novacoin.org/", 
+"POT":"http://potcoin.info/", "ANC":"https://anoncoin.net/", "DGB":"http://www.digibyte.co/",
+"RDD":"http://www.reddcoin.com/", "HBN":"http://hobonickels.info/", "CRYPT":"http://cryptco.org/", 
+"RZR":"https://bitcointalk.org/index.php?topic=644498.0", "NAUT":"http://www.nautiluscoin.com/"}
+SCAMS = ["CRYPT", "CGB", "IFC", "IXC", "RZR"]
+NOT_MINED = ['VOOT']
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+	def valid_user(self):
+		user = self.request.cookies.get('user')
+		if user:
+			user = check_secure_val(self.request.cookies.get('user'))
+			return user
+		else:
+			return False
+
+def get_url(url):
+	try:
+		r = requests.get(url, timeout=5)
+	except (ConnectionError, 'Timeout') as e:
+		r = False  #'ConnectionFailed'
+		return r
+	try:
+		r.raise_for_status()
+	except requests.exceptions.HTTPError:
+		r = False
+	return r
+
+def marketdata(update=False):
+	url = "http://pubapi.cryptsy.com/api.php?method=marketdatav2"
+	data = memcache.get("marketdata")
+	if data == None or update:
+		data = get_url(url)
+		if data:
+			data = data.json()
+			cacheTime = int(time.time())
+			newData = {}
+			for i in data['return']['markets']:
+				if data['return']['markets'][i]['lasttradeprice']:
+					newData[i] = data['return']['markets'][i]['lasttradeprice']
+			BTC_USD = get_url('http://api.coindesk.com/v1/bpi/currentprice/USD.json')
+			BTC_USD = BTC_USD.json()
+			BTC_USD = float(BTC_USD['bpi']['USD']['rate'])
+			newData['BTC/USD'] = BTC_USD
+			memcache.set("marketdata", [newData, cacheTime])
+			return newData, cacheTime
+		else:
+			return False
+	if ((int(time.time()) - data[1]) > 600):
+		return marketdata(update=True)
+	else:
+		return data
+
+def coins_per_block(update=False):
+	url = "http://www.coinwarz.com/v1/api/profitability/?apikey=25ea0abd0d334c059fff797b5ea80272&algo=all"
+	data = memcache.get("coindata")
+	if data == None or update:
+		data = get_url(url)
+		if data:
+			data = data.json()
+			cacheTime = int(time.time())
+			newData = {}
+			i = 0
+			while i < len(data['Data']):
+				if data['Data'][i]["CoinTag"] not in ['ZEIT', 'CGB', "IFC", 'VOOT']:
+					newData[data['Data'][i]["CoinTag"]] = [data['Data'][i]['BlockReward'], data['Data'][i]['BlockTimeInSeconds'], data['Data'][i]['CoinName'] ,data['Data'][i]['ExchangeRate']]
+				i += 1
+			amounts = other_amounts()
+			if amounts:
+				for i in amounts:
+					newData[i] = amounts[i]
+			memcache.set("coindata", [newData, cacheTime])
+			if memcache.get("CPB_last_attempt"):
+				memcache.delete("CPB_last_attempt")
+			return newData, cacheTime
+		else:
+			return False
+	if ((int(time.time()) - data[1]) > 86400):
+		if memcache.get("CPB_last_attempt") and (int(time.time()) - memcache.get("CPB_last_attempt") < 21600):
+			return data
+		else:
+			return coins_per_block(update=True)
+	else:
+		return data
+
+def other_amounts():
+	amounts = {}
+	r = get_url("https://coinplorer.com/XPM")
+	if r:
+		text = r.text
+		reg = re.compile("(?s)Block reward:</td>.+<td>(\d+\.\d+)</td>")
+		match = re.search(reg, text)
+#		if match:
+		reward = match.group(1)
+		amounts["XPM"] = [float(reward), 60, 'Primecoin']
+	f = get_url("https://coinplorer.com/FLO")
+	if f:
+		text = f.text
+		reg = re.compile("(?s)Block reward:</td>.+<td>(\d+\.\d+)</td>")
+		match = re.search(reg, text)
+#		if match:
+		reward = match.group(1)
+		amounts["FLO"] = [float(reward), 40, 'Florincoin']
+	return amounts
+
+def get_and_verify_sources():
+	data = marketdata()
+	coins_block = coins_per_block()
+	if data == False:
+		return False
+	elif coins_block == False:
+		if memcache.get("coindata") and (int(time.time()) - memcache.get("coindata")[1] < 604800):
+			memcache.set("CPB_last_attempt", int(time.time()))
+			coins_block = memcache.get("coindata")
+		else:
+			return False
+	return data, coins_block
+
+class GetPubAPI(Handler):
+	def get(self):
+		source = get_and_verify_sources()
+		if source:
+			data, coins_block = source
+			USD_data = USD_price_calc(data)
+			coin_data = {} #USD per day
+			#debug use
+			unknown_price = []
+			unknown_amount = []
+			#end debug
+			for i in coins_block[0]:
+				if USD_data.get(i + "/USD"):
+					coin_data[i] = coins_block[0][i][0] * (86400 / coins_block[0][i][1]) * USD_data[i + "/USD"]
+				else: unknown_price = unknown_price + [i]
+			#more debug
+			for j in USD_data:
+				t = False
+				for k in coins_block[0]:
+					if k == j[:-4]:
+						t = True
+						break
+				if t == False:
+					unknown_amount = unknown_amount + [j[:-4]]
+			#end debug
+			# Find total USD per day
+			total = 0.0
+			for item in coin_data:
+				total += coin_data[item]
+			totalUSD = "${:,.2f}".format(total)
+			#sort and format
+			coin_data = sorted(coin_data.iteritems(), key=operator.itemgetter(1), reverse=True)
+			l = 0
+			while l < len(coin_data):
+				coin_data[l] = [coins_block[0][coin_data[l][0]][2], "${:,.2f}".format(coin_data[l][1]), coin_data[l][0], "%.7f" % (coin_data[l][1] * 100 / total)] # CoinName, USD per Day, CoinTag, % of total USD
+				l += 1
+			#self.write(unknown_amount)
+			#self.write(unknown_price)
+			self.render('bitcoin_data.html', data=coin_data, coin_urls=COIN_URLS, scams=SCAMS, time=time.strftime("%b %d %Y", time.gmtime()), totalUSD=totalUSD)
+		else:
+			self.write("Error: Price Data Unavialable")
+			#self.write(marketdata())
+
+
+
+def USD_price_calc(marketdata):
+	BTC_USD = marketdata[0]['BTC/USD']
+	LTC_USD = float(marketdata[0]['LTC/BTC']) * BTC_USD
+	marketdata = marketdata[0]
+	USD_prices = {}
+	for i in marketdata:
+		if i[-3:] == 'BTC':
+			if i[:-3] + 'USD' not in USD_prices:
+				USD_prices[i[:-3] + 'USD'] = float(marketdata[i]) * BTC_USD
+		elif i[-3:] == 'LTC':
+			if i[:-3] + 'USD' not in USD_prices:
+				USD_prices[i[:-3] + 'USD'] = float(marketdata[i]) * LTC_USD
+		elif i[-3:] == 'USD':
+			USD_prices[i] = float(marketdata[i])
+	USD_prices['BTC/USD'] = BTC_USD
+#	for i in USD_prices:
+#		if USD_prices[i] < 1:
+#		USD_prices[i] = format(USD_prices[i], 'f', precision=8).rstrip('0')
+	return USD_prices
+
+def daily_yield(USD_prices):
+	USD_amounts = {}
+	for item in DAILY_AMOUNTS:
+		USD_amounts[item] = USD_prices[item + '/USD'] * DAILY_AMOUNTS[item]
+	return USD_amounts
+
+class GetJSON(Handler):
+	def get(self):
+		self.response.headers['Content-Type'] = 'application/json'
+		source = get_and_verify_sources()
+		if source:
+			data, coins_block = source
+			USD_data = USD_price_calc(data)
+			coin_data = {} #USD per day
+			for i in coins_block[0]:
+				if USD_data.get(i + "/USD"):
+					coin_data[i] = coins_block[0][i][0] * (86400 / coins_block[0][i][1]) * USD_data[i + "/USD"]	
+
+			total = 0.0
+			for item in coin_data:
+				total += coin_data[item]
+			totalUSD = round(total, 2)
+
+			coin_data = sorted(coin_data.iteritems(), key=operator.itemgetter(1), reverse=True)
+
+			l = 0
+			while l < len(coin_data):
+				coin_data[l] = {"CoinName":coins_block[0][coin_data[l][0]][2], "USD_per_day":round(coin_data[l][1], 2), "CoinTag":coin_data[l][0], "PercentOfTotalUSD":(coin_data[l][1] / total)} # CoinName, USD per Day, CoinTag, % of total USD
+				l += 1
+			finalJSON = {"success":1, "return":coin_data}
+			self.write(finalJSON)
+		else:
+			finalJSON = {"success":0}
+			self.write(finalJSON)
+
+app = webapp2.WSGIApplication([
+    (r'/', GetPubAPI),
+    (r'/json', GetJSON)
+], debug=True)
+
